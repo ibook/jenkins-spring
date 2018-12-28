f88ce49f99b03001af563b29e284aa3416e5ed30
Neo Chan <netkiller@msn.com>
2018-12-28 17:46:42 +0800

    Add junit

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,12 @@ public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertEquals(5,5);
+	}
+	
+	@Test
+	public void failures() {
+		Assert.assertEquals(10,10);
 	}
 
 }
